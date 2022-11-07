26e710f12dfe86c854431a3a2a424f3d68244944
cherish-lin <75791396+cherish-lin@users.noreply.github.com>
2022-11-07 18:44:30 +0800

    Update and rename EulerOperation.cpp to EulerOperator.cpp

--- EulerOperator.cpp
+++ EulerOperator.cpp
@@ -3,14 +3,14 @@
 HalfEdge* EulerOperator::mev(Vertex* sv, double point[3], Loop* loop)
 {
 	Solid* solid = loop->face->solid;
-	Edge* edge = new Edge();//create a new edge
+	Edge* edge = new Edge();
 	HalfEdge* half_l = new HalfEdge();
 	HalfEdge* half_r = new HalfEdge();
 	Vertex* ev = new Vertex(point[0], point[1], point[2]);
 
 	ev->id = solid->vnum;
-	v_list.push_back(ev);//store the vertex by order
-	solid->vnum += 1;//remember to increase the vertex num of the solid
+	v_list.push_back(ev);
+	solid->vnum += 1;
 	half_l->startVertex = sv;
 	half_l->endVertex = ev;
 	half_r->startVertex = ev;
@@ -27,7 +27,6 @@ HalfEdge* EulerOperator::mev(Vertex* sv, double point[3], Loop* loop)
 	half_l->lp = loop;
 	half_r->lp = loop;
 
-	//add the new two halfedges into the loop
 	if (loop->halfedges == NULL)
 	{
 		half_l->next = half_r;
@@ -53,12 +52,11 @@ HalfEdge* EulerOperator::mev(Vertex* sv, double point[3], Loop* loop)
 		half_r->pre = half_l;
 	}
 	
-	//add the edge into the edge list of solid
 	addEdgeIntoSolid(edge, solid);
 	return half_l;
 }
 
-Face* EulerOperator::mef(Vertex* sv, Vertex* ev, Loop* loop, bool mark)
+Face* EulerOperator::mef(Vertex* sv, Vertex* ev, Loop* loop, bool flag)
 {
 	Solid* solid = loop->face->solid;
 	Edge* edge = new Edge();
@@ -80,7 +78,6 @@ Face* EulerOperator::mef(Vertex* sv, Vertex* ev, Loop* loop, bool mark)
 	edge->r = half_r;
 
 
-	//add the new two halfedge into the loop
 	HalfEdge* thalf = loop->halfedges;
 	HalfEdge* tmpa, * tmpb, * tmpc;
 	while (thalf->endVertex != sv)thalf = thalf->next;
@@ -93,7 +90,6 @@ Face* EulerOperator::mef(Vertex* sv, Vertex* ev, Loop* loop, bool mark)
 	while (thalf->endVertex != ev)thalf = thalf->next;
 	tmpc = thalf;
 
-	//divide the big loop into two small loop
 	half_r->next = tmpa->next;
 	tmpa->next->pre = half_r;
 	tmpa->next = half_l;
@@ -113,14 +109,13 @@ Face* EulerOperator::mef(Vertex* sv, Vertex* ev, Loop* loop, bool mark)
 	newLoop->id = solid->lnum;
 	solid->lnum += 1;
 	l_list.push_back(newLoop);
-	//add face into the face list of solid
 	addFaceIntoSolid(face, solid);
 
 	addLoopIntoFace(newLoop, face);
 
 	if (tmpc == tmpb)
 	{
-		if (mark)//only the face in the bottom 
+		if (flag)
 		{
 			sweep_list.push_back(half_l->lp->face);
 		}
@@ -130,7 +125,6 @@ Face* EulerOperator::mef(Vertex* sv, Vertex* ev, Loop* loop, bool mark)
 		sweep_list.push_back(half_r->lp->face);
 	}
 
-	//add edge into the edge list of solid
 	addEdgeIntoSolid(edge, solid);
 
 	return face;
@@ -148,11 +142,12 @@ Solid* EulerOperator::mvfs(double point[3], Vertex*& vertex)
 	face->id = solid->fnum;
 
 	l_list.push_back(out_lp);
-	//printf("%lf %lf %lf\n", vertex->coordinate[0], vertex->coordinate[1], vertex->coordinate[2]);
-	v_list.push_back(vertex);//store the vertex by order
-	solid->vnum += 1;//increase the num of vertexs
-	solid->fnum += 1;//increase the num of faces
-	solid->lnum += 1;//increase the num of loops
+	
+	v_list.push_back(vertex);
+
+	solid->vnum += 1;
+	solid->fnum += 1;
+	solid->lnum += 1;
 
 
 	solid->faces = face;
@@ -195,7 +190,6 @@ Loop* EulerOperator::kemr(Vertex* sv, Vertex* ev, Loop* loop)
 	l_list.push_back(inlp);
 
 	addLoopIntoFace(inlp, tmpa->pre->brother->lp->face);
-	//addLoopIntoFace(inlp, face);
 	delete tmpa;
 	delete tmpb;
 
@@ -234,21 +228,18 @@ void EulerOperator::sweep(double dir[3], double dist)
 	HalfEdge* he, * suphe, * uphe;
 	double point[3];
 
-	vector<Face*>::iterator ite;
-	for (ite = sweep_list.begin(); ite != sweep_list.end(); ++ite)
+	vector<Face*>::iterator it;
+	for (it = sweep_list.begin(); it != sweep_list.end(); it++)
 	{
-
-		//solve the first vertex when process the sweeping operator
-		Loop* loop = (*ite)->out_lp;
+		Loop* loop = (*it)->out_lp;
 		he = loop->halfedges;
 		startv = he->startVertex;
 		point[0] = startv->positions[0] + dist * dir[0];
 		point[1] = startv->positions[1] + dist * dir[1];
 		point[2] = startv->positions[2] + dist * dir[2];
 
-
-		suphe = mev(startv, point, loop);//acquire the first down_to_up halfedge
-		upprev = suphe->endVertex;//record the fist up vertex as the pre vertex
+		suphe = mev(startv, point, loop);
+		upprev = suphe->endVertex;
 		he = he->next;
 		nextv = he->startVertex;
 
@@ -292,11 +283,11 @@ void EulerOperator::addEdgeIntoSolid(Edge* edge, Solid*& solid)
 void EulerOperator::addFaceIntoSolid(Face* face, Solid*& solid)
 {
 	Face* tface = solid->faces;
-	if (tface == NULL)
+	if (tface == NULL) //第一个面
 	{
 		solid->faces = face;
 	}
-	else
+	else //把当前面插在面链表的最后
 	{
 		while (tface->next != NULL)tface = tface->next;
 		tface->next = face;
@@ -306,28 +297,27 @@ void EulerOperator::addFaceIntoSolid(Face* face, Solid*& solid)
 
 	face->id = solid->fnum;
 
-	solid->fnum += 1;// increase the num of faces
+	solid->fnum += 1;
 }
 
 void EulerOperator::addLoopIntoFace(Loop* loop, Face*& face)
 {
 	loop->face = face;
-
-	//there is only one out loop but there may have lots of inner loop
-	if (face->out_lp == NULL)
+	// 只有一个外环，有多个内环
+	if (face->out_lp == NULL) //外环
 	{
 		face->out_lp = loop;
 	}
-	else
+	else //内环
 	{
 		//cout << "in loop" << endl;
-		Loop* tlp = face->inner_lp;
-		if (tlp == NULL)face->inner_lp = loop;
+		Loop* lp = face->inner_lp;
+		if (lp == NULL)face->inner_lp = loop;
 		else
 		{
-			while (tlp->next != NULL)tlp = tlp->next;
-			tlp->next = loop;
-			loop->pre = tlp;
+			while (lp->next != NULL)lp = lp->next;
+			lp->next = loop;
+			loop->pre = lp;
 		}
 		face->inum += 1;
 		//cout << face->id << ' ' << face->inum << "!!!!!!!!" << endl;
